e54a8e0bb9e9cdf44e26ce6688ab733f6247a55a
Andrea <amartin.onda@gmail.com>
2021-12-17 13:43:23 +0100

    initial commit

---
+++ JQuery/js/ejercicio1.js
@@ -0,0 +1,13 @@
+$(document).ready(()=>{
+
+    var divs = $("div");
+
+    alert("Hay "+divs.length+" capas en la página");
+    
+    divs.css("color", "green");
+
+});
+
+
+
+
---
+++ JQuery/js/ejercicio2.js
@@ -0,0 +1,15 @@
+$(document).ready(()=>{
+
+    $("#muestra").click(()=>{
+
+        $("img").show(2500);
+
+    });
+
+    $("#oculta").click(()=>{
+
+        $("img").hide("slow");
+
+    });
+
+});
