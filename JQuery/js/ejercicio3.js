6115372e33c0362023593dba49c0528d075d71ec
Andrea <amartin.onda@gmail.com>
2021-12-17 13:49:50 +0100

    cambios17Dic

--- JQuery/js/ejercicio3.js
+++ JQuery/js/ejercicio3.js
@@ -1,5 +1,21 @@
 $(document).ready(()=>{
 
-    
+    $("#btnRojo").change(()=>{
+
+        $("#parrafo").css("color", "red");
+
+    });
+
+    $("#btnAzul").change(()=>{
+
+        $("#parrafo").css("color", "blue");
+
+    });
+
+    $("#btnVerde").change(()=>{
+
+        $("#parrafo").css("color", "green");
+
+    });
 
 });
