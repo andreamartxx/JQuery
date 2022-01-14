72ff416977e66e691946661fab72dbda29eab7a4
Andrea <amartin.onda@gmail.com>
2022-01-14 13:15:01 +0100

    cambios14E

--- js/ejercicio4_ajax.js
+++ js/ejercicio4_ajax.js
@@ -1,6 +1,8 @@
 $(document).ready(()=>{
     $("#btn").click(()=>{
-        $.post( "script.php", {nombre: "Andrea", apellidos: "Martin"}).done(function(data) {
+        $.post( "script.php", 
+        {nombre: "Andrea", apellidos: "Martin"})
+        .done(function(data) {
             $("#container").html(data);
         })
     });
