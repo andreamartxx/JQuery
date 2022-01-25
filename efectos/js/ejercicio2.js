8155fb0f0d360f9c97b1af2ac818ad6162108be2
Andrea <amartin.onda@gmail.com>
2022-01-25 13:18:24 +0100

    cambios25E

--- efectos/js/ejercicio2.js
+++ efectos/js/ejercicio2.js
@@ -2,13 +2,13 @@ $(document).ready(()=>{
 
     $("#btn").click(()=>{
 
-        $("h1").fadeTo(1500, 0.2);
+        $("h1").fadeTo(1500, 1);
 
     });
 
     $("#btn1").click(()=>{
 
-        $("h1").fadeTo(3000, 1, ()=>{
+        $("h1").fadeTo(2500, 0.1, ()=>{
             alert("Hola :)");
         });
 
